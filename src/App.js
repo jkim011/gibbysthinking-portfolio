79baaf674174a027375ee3f6d3ef1fdec166500f
Jared Kim <jaredkim01@yahoo.com>
2023-04-15 00:02:45 -0500

    files added

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App page-container">
+      <div className='content-wrap'>
+        Hello world
+      </div>
+    </div>
+  );
+}
+
+export default App;
