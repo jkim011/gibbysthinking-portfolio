13584826ae1056d68b802718b97bebf3825f38c1
Jared Kim <jaredkim01@yahoo.com>
2023-04-16 14:47:39 -0500

    nav bar functions

--- src/App.js
+++ src/App.js
@@ -4,12 +4,18 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import Header from './components/Header';
+import About from './components/About';
+import Portfolio from './components/Portfolio';
+import Contact from './components/Contact';
 
 function App() {
   return (
     <div className="App page-container">
       <div className='content-wrap'>
         <Header />
+        <About />
+        <Portfolio />
+        <Contact />
       </div>
     </div>
   );
---
+++ src/components/Contact.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+function Contact() {
+  return (
+    <div id="contact">
+      Contact Me
+    </div>
+  )
+}
+
+export default Contact;
