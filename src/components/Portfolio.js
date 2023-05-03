71e0d301bd5e19cbda649a89a8b0dd236af09d2e
Jared Kim <jaredkim01@yahoo.com>
2023-05-02 22:20:06 -0500

    updated art, changed media query mobile

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -5,6 +5,7 @@ import Modal from 'react-bootstrap/Modal';
 import Concha from '../assets/art/concha.png';
 import Paleta from '../assets/art/paleta-payaso.png';
 import Bump from '../assets/art/bump.png';
+import BumpBlue from '../assets/art/bumpBlue.png';
 import Fresas from '../assets/art/fresas.png';
 import Cake from '../assets/art/cake.jpg';
 import Conchita from '../assets/art/conchita.png';
@@ -13,7 +14,7 @@ function Portfolio() {
   const [conchaShow, setConchaShow] = useState(false);
   const [paletaShow, setPaletaShow] = useState(false);
   const [fresasShow, setFresasShow] = useState(false);
-  const [bumpShow, setBumpShow] = useState(false);
+  const [bumpBlueShow, setBumpBlueShow] = useState(false);
   const [cakeShow, setCakeShow] = useState(false);
   const [conchitaShow, setConchitaShow] = useState(false);
 
@@ -23,7 +24,7 @@ function Portfolio() {
         <img src={Concha} alt="" className="art art-dimensions box-shadow" onClick={() => setConchaShow(true)}/>
         <img src={Paleta} alt="" className="art art-dimensions box-shadow" onClick={() => setPaletaShow(true)}/>
         <img src={Fresas} alt="" className="art art-dimensions box-shadow" onClick={() => setFresasShow(true)}/>
-        <img src={Bump} alt="" className="art art-dimensions box-shadow" onClick={() => setBumpShow(true)}/>
+        <img src={BumpBlue} alt="" className="art art-dimensions box-shadow" onClick={() => setBumpBlueShow(true)}/>
         <img src={Cake} alt="" className="art art-dimensions box-shadow" onClick={() => setCakeShow(true)}/>
         <img src={Conchita} alt="" className="art art-dimensions box-shadow" onClick={() => setConchitaShow(true)}/>
       </div>
@@ -38,8 +39,8 @@ function Portfolio() {
       <Modal className="art-modals" size="lg" show={fresasShow} onHide={() => setFresasShow(false)} aria-labelledby="example-modal-sizes-title-lg" centered>
         <img src={Fresas}/>
       </Modal>
-      <Modal className="art-modals" size="lg" show={bumpShow} onHide={() => setBumpShow(false)} aria-labelledby="example-modal-sizes-title-lg" centered>
-        <img src={Bump}/>
+      <Modal className="art-modals" size="lg" show={bumpBlueShow} onHide={() => setBumpBlueShow(false)} aria-labelledby="example-modal-sizes-title-lg" centered>
+        <img src={BumpBlue}/>
       </Modal>
       <Modal className="art-modals" size="lg" show={cakeShow} onHide={() => setCakeShow(false)} aria-labelledby="example-modal-sizes-title-lg" centered>
         <img src={Cake}/>
