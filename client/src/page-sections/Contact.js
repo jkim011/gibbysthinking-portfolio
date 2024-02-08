0aeb5770df6104a7cba622e5967e08edbbe3c71f
Jared Kim <jaredkim01@yahoo.com>
2024-02-07 23:30:48 -0600

    adjusted media queries

--- client/src/page-sections/Contact.js
+++ client/src/page-sections/Contact.js
@@ -10,11 +10,7 @@ function Contact() {
       <div className="contact-links">
         <a href="https://www.instagram.com/gibbysthinking" target="_blank" rel="noreferrer"><img className="socials" src={IgIcon}/></a>
         <a href="https://www.tiktok.com/@gibbysthinking" target="_blank" rel="noreferrer"><img className="socials" src={TiktokIconShape}/></a>
-        <a href="https://www.twitter.com/gibbysthinking" target="_blank" rel="noreferrer"><img className="socials" src={TwitterIcon}/></a>
         <a href="mailto: gabbypduran@gmail.com" target="_blank" rel="noreferrer"><img className="socials" src={EmailIcon}/></a>
-      </div>
-      <div style={{marginTop:"10px", marginBottom:"15px"}}>
-        <p className="body-text bold-text">Art by Gabrielle Duran</p>
       </div>
     </div>
   )
