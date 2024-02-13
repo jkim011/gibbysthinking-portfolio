fa724e512d2e199d335e57847a2c099b99d27a72
Jared Kim <jaredkim01@yahoo.com>
2024-02-13 17:00:03 -0600

    style changes, added name logo

--- client/src/page-sections/About.js
+++ client/src/page-sections/About.js
@@ -2,18 +2,20 @@ import React from "react";
 
 import ProfileAvatar from '../assets/profile-avatar.png';
 import GabbyAvatar from '../assets/gabby-avatar-cropped.png';
+import NameLogo from '../assets/name-logo.png';
 import Rays from '../assets/rays.png';
 
 function About() {
   return (
     <div id="about">
-        <div>
-          {/* <img src={Rays} className="rays"/>  */}
-        </div>
+        {/* <div>
+          <img src={Rays} className="rays"/> 
+        </div> */}
         <div>
           <img src={GabbyAvatar} className="profile-avatar box-shadow"/> 
         </div>
       <div className="about-text">
+        <img src={NameLogo} className="name-logo"/>
         <p>We have the ability to trigger deep memories through food; Its texture, taste, and image can bring back emotions that we experienced long ago. My focus is to create pieces that can remind us of these bygone memories, & for these feelings of nostalgia to connect us closer as we find out just how much we shared in our childhood.</p>
       </div>
       
