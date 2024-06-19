0d955dca9a0a519be57daab987f8ed17b8d1f44d
Jared Kim <jaredkim01@yahoo.com>
2024-06-18 21:44:02 -0500

    starting contact form

--- client/src/page-sections/Contact.js
+++ client/src/page-sections/Contact.js
@@ -1,10 +1,24 @@
 import React from "react";
 
-// Possibly add form for ppl to request something custom
 function Contact() {
   return (
     <div id="contact">
-      contact me!
+      <h1 className="mb-4">Contact Me</h1>
+      <form id="contact-form" >
+        <div className="form-group">
+          <label>Name</label> <br/>
+          <input type="text" className=""/>
+        </div>
+        <div className="form-group">
+          <label>Email address</label> <br/>
+          <input type="email" className="" />
+        </div>
+        <div className="form-group">
+          <label>Message</label> <br/>
+          <textarea className="" rows={6} />
+        </div>
+        <button type="submit" className="btn btn-primary">Submit</button>
+      </form>
     </div>
   )
 }
